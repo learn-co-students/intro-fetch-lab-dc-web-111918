ad8fcc4304a221598d378d658f886fdbab8c2bfc
Manon Pilaud <mandyypilaud@gmail.com>
2019-01-13 21:25:36 -0500

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,83 @@
-// Write your numbers code in this file!
+document.addEventListener('DOMContentLoaded', function() {
+  whatYear()
+})
+
+function choice(){
+  return document.querySelector('#pick-a-number')
+}
+
+function randomMathFact(){
+  return document.querySelector('#random-math-fact')
+}
+
+function yearHistory(){
+  return document.querySelector('#year-history')
+}
+
+function hundredNumbers(){
+  return document.querySelector('#all-the-numbers')
+}
+function whatYear(){
+  let year = new Date().getFullYear()
+  currentYearFact(year)
+  setInterval(function(){
+    year --
+    currentYearFact(year)
+  },5000)
+}
+
+function currentYearFact(year){
+  fetch(`http://numbersapi.com/${year}/trivia`)
+  .then(response=>response.text())
+  .then(data=>{
+    yearHistory().innerHTML = data
+  })
+}
+
+
+
+document.querySelector('#number-one').addEventListener("click",function(){
+  numberOne()
+})
+
+function numberOne(){
+  fetch('http://numbersapi.com/1/trivia')
+  .then (response => response.text())
+  .then(data =>{
+  document.querySelector('#one-facts').innerHTML = data
+  })
+}
+
+choice().addEventListener("change",function(e){
+  e.preventDefault
+  let number = choice().value
+  if (isNaN(number)) {
+      randomMathFact().innerHTML= ('please enter a valid number')
+    } else {
+      pickNumber(number)
+    }
+})
+
+function pickNumber(number){
+  fetch(`http://numbersapi.com/${number}/trivia`)
+  .then(response=>response.text())
+  .then(data =>{
+  randomMathFact().innerHTML = data
+  })
+}
+document.querySelector('#all-numbers-button').addEventListener('click',function(){
+  getAllNumbers()})
+
+
+function getAllNumbers(){
+  fetch('http://numbersapi.com/1..100')
+  .then(response=>response.json())
+  .then(numbers => {
+    let html = '<ul>'
+    for (key in numbers) {
+      html += `<li>${numbers[key]}</li>`
+    }
+    html += '</ul>'
+    hundredNumbers().innerHTML = html
+  })
+}
--- swapi.js
+++ swapi.js
@@ -1,1 +1,121 @@
-// Write your swapi code in this file!
+function crawl() {
+let crawlButton = document.querySelector('#crawlBtn')
+crawlButton.addEventListener('click', function(){
+  getOpeningCrawl()
+})
+}
+function getOpeningCrawl() {
+  let crawlDiv = document.querySelector('#crawlDiv')
+  fetch('https://swapi.co/api/films/1/')
+  .then(response => response.json())
+  .then(jsonData => {
+    crawlDiv.innerHTML = jsonData.opening_crawl
+  })
+  .catch(error => {alert('Something went wrong')})
+}
+function getPlanetForm() {
+  let formPlanet = document.getElementById('planetForm')
+  formPlanet.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let input = document.getElementById('planetInput')
+    let inputNum = parseInt(input.value)
+    if (inputNum > 0 && inputNum < 62) {
+      getPlanet(inputNum)
+    } else {
+      alert("Not a valid planet Id")
+    }
+  })
+}
+function getPlanet(inputNum) {
+  fetch(`https://swapi.co/api/planets/${inputNum}`)
+  .then(response => response.json())
+  .then(jsonData => {
+    planetData.innerHTML = `Planet Name: ${jsonData.name}, Climate: ${jsonData.climate}`
+  })
+  .catch(error => {alert('Something went wrong')})
+}
+crawl();
+getPlanetForm();
+
+function droidTwo(){
+  return document.querySelector('#droid-2')
+}
+function droidThree(){
+  return document.querySelector('#droid-3')
+}
+
+function createButton(droid, droidId, droidNumber,divId,planetId){
+  button = document.createElement("button");
+  button.id = droidId
+  button.innerText = droidNumber
+  droid.appendChild(button)
+  info = document.createElement("div")
+  info.id = divId
+  droid.appendChild(info)
+  planet = document.createElement("div")
+  planet.id = planetId
+  droid.appendChild(planet)
+}
+
+//------------------------Droid Stuff-------------------------------------
+createButton(droidTwo(),"droidTwo","Droid thing two","DroidTwoInfo","planetTwo")
+createButton(droidThree(),"droidThree","Droid thing Three","DroidThreeInfo","planetThree")
+
+droidTwo().addEventListener('click', function(){
+  getDroidTwo()
+  homePlanetDroidTwo()
+})
+
+function getDroidTwo(){
+  fetch('https://swapi.co/api/people/2')
+  .then(response=>response.json())
+  .then(jsonData=>{
+    document.querySelector('#DroidTwoInfo').innerHTML = `Name: ${jsonData.name},  Mass: ${jsonData.mass}, height: ${jsonData.height}`
+  })
+  .catch(error=>{alert("Something went wrong")})
+}
+
+function homePlanetDroidTwo(){
+fetch('https://swapi.co/api/planets/1/')
+.then(response=>response.json())
+.then(jsonData=>{
+document.querySelector('#planetTwo').innerHTML = `Home Planet: ${jsonData.name}, Terrain: ${jsonData.terrain}, Population: ${jsonData.population}`
+})
+.catch(error=>{alert("something went wrong")})
+}
+
+
+//--------------------------Droid3----------------------------------------
+droidThree().addEventListener('click', function(){
+  getDroidThree()
+  homePlanetDroidThree()
+})
+
+function getDroidThree(){
+  fetch('https://swapi.co/api/people/3')
+  .then(response=>response.json())
+  .then(jsonData=>{
+    document.querySelector('#DroidThreeInfo').innerHTML = `Name: ${jsonData.name},  Mass: ${jsonData.mass}, height: ${jsonData.height}`
+  })
+  .catch(error=>{alert("Something went wrong")})
+}
+
+function homePlanetDroidThree(){
+  fetch('https://swapi.co/api/planets/8/')
+  .then(response=>response.json())
+  .then(jsonData=>{
+  document.querySelector('#planetThree').innerHTML = `Home Planet: ${jsonData.name}, Terrain: ${jsonData.terrain}, Population: ${jsonData.population}`
+  })
+  .catch(error=>{alert("something went wrong")})
+}
+
+
+
+
+  //get info name, height, mass
+  // when button is clicked home planet
+        //get Api and info for home planet
+
+  //get info name, height, mass
+  // when button is clicked home planet
+      //get Api and info for home planet
